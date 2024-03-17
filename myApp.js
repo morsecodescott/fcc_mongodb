154abe878ceb23da40d94b3fe63c1ba5721a32a0
Scott Morse <scott.m.morse@gmail.com>
2024-03-17 02:22:25 +0000

    test

--- myApp.js
+++ myApp.js
@@ -1,9 +1,18 @@
 require('dotenv').config();
-mongoose = require('mongoose');
+let mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+let personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+        },
+  age: Number,
+  favoriteFoods : [String]
+  });
+
+let Person = mongoose.model('Person' personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
